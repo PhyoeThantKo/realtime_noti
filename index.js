3afdbef81e6824d42b69d0ab3bf41441567b419b
Phyoe Thant Ko <84618795+PhyoeThantKo@users.noreply.github.com>
2022-07-22 10:00:11 +0630

    foundation and server setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Socket } = require('socket.io')
+
+const app = require('express')()
+const httpServer = require('http').createServer(app)
+const io = require('socket.io')(httpServer)
+
+app.get('/', (req, res) => {
+     res.sendfile('index.html')
+})
+
+app.get('/admin', (req, res) => {
+     res.sendfile('admin.html')
+})
+
+io.on('connection', (socket) => {
+     console.log("User : " + socket.id)
+})
+
+httpServer.listen(3000, ()=>{
+     console.log("Server running at port 3000...")
+})
